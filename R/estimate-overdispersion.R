5a9b7146c9bcccadaee218c3fc6deb946deff019
Spencer Fox <spncrfx@gmail.com>
2023-07-17 11:26:58 -0600

    update with explanation

--- R/estimate-overdispersion.R
+++ R/estimate-overdispersion.R
@@ -1,3 +1,9 @@
+## Code to estimate an overdispersion parameter for R0
+## Based on 20/80 rule from: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3128496/
+## and https://www.nature.com/articles/438293a
+
+## Converts that to overdispersion parameter for a specific R0 using the method from:
+## https://onlinelibrary.wiley.com/doi/10.1111/tbed.14655
 
 library(tidyverse)
 
